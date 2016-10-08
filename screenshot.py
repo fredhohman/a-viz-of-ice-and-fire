b0cb90335f10f4ea15bb9c9be9a5f089b465d860
Fred Hohman <fred.hohman@gmail.com>
2016-10-08 19:44:53 -0400

    color code works by importing directory of images and draws color palette figure

--- screenshot.py
+++ screenshot.py
@@ -5,8 +5,8 @@ import time
 fred_ehdd = '/Users/fredhohman/Desktop/temp/'
 
 # Define `timeout` for fail safe, don't want to fill up harddrive with screenshots!
-num_secs = 20
-num_mins = 0
+num_secs = 0
+num_mins = 1
 num_hours = 0
 
 timeout = time.time() + num_secs + 60*num_mins + 3600*num_hours # add time in seconds
@@ -15,6 +15,6 @@ if __name__ == '__main__':
 	while True:
 		date = time.strftime("%H-%M-%S_%m-%d-%y")
 		call(['screencapture', fred_ehdd + str(date) + '.png'])
-		time.sleep(1) # screenshot every ~1 second
+		time.sleep(1.0) # screenshot every ~1 second
 		if time.time() > timeout:
 			break
