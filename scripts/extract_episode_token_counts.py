2ebb21ab14cec75ea3a5f01703eb209b1520e4ef
istewart6 <istewart6@gatech.edu>
2016-10-10 15:10:30 -0400

    tweaking token counts

--- scripts/extract_episode_token_counts.py
+++ scripts/extract_episode_token_counts.py
@@ -3,13 +3,15 @@ Extract token counts for each episode and
 write to one big ugly document term matrix.
 """
 import pandas as pd
-import json, os
+import json, os, re
 from clean_extracted_text import clean_text
 from sklearn.feature_extraction.text import CountVectorizer
 
 if __name__ == '__main__':
     subtitle_dir = '../data/subtitles/subtitlesInTSV/'
-    sorted_episodes = sorted(os.listdir(subtitle_dir))
+    all_episodes = [f for f in os.listdir(subtitle_dir) 
+                    if re.findall('S[0-9]E[0-9]+', f)]
+    sorted_episodes = sorted(all_episodes)
     episode_data = {e : pd.read_csv(os.path.join(subtitle_dir, e), sep='\t') 
                     for e in sorted_episodes}
     all_counts = {}
@@ -21,7 +23,7 @@ if __name__ == '__main__':
         e_data = episode_data[e]
         all_dialogue = []
         for d in e_data['dialogue']:
-            clean_dialogue = clean_text(str(d)).replace('$', '')
+            clean_dialogue = clean_text(str(d)).replace('$', ' ').strip()
             
             if(clean_dialogue != ''):
                 try:
@@ -31,7 +33,7 @@ if __name__ == '__main__':
                     print('clean dialogue %s caused error %s'%(clean_dialogue, e))
         counts = cv.fit_transform(all_dialogue)
         # only care about per-episode counts
-        count_sums = list(pd.np.array(counts.sum(axis=1))[0])
+        count_sums = list(pd.np.array(counts.sum(axis=0))[0])
         sorted_vocab = sorted(cv.vocabulary_.keys(), key=lambda x: cv.vocabulary_[x])
         # make count dict from sorted vocab and sum counts
         count_dict = dict(zip(sorted_vocab, count_sums))
@@ -39,9 +41,10 @@ if __name__ == '__main__':
     # now combine all counts
     combined_vocab = list(reduce(lambda x,y: x.union(y),
                                  [set(d.keys()) for d in all_counts.values()]))
+    print('combined vocab %s'%(combined_vocab))
     all_counts_dfs = [pd.DataFrame(d, index=[e]).transpose() 
                       for e, d in all_counts.items()]
     combined_counts_df = pd.concat(all_counts_dfs, axis=1)
     combined_counts_df.fillna(0, inplace=True)
     out_dir = subtitle_dir
-    combined_counts_df.to_csv(os.path.join(out_dir, 'episode_token_counts.tsv'), sep='\t')
+    combined_counts_df.to_csv(os.path.join(out_dir, 'episode_token_counts.tsv'), sep='\t', encoding='utf-8')
