67f25c20de2a0d25ebc2096e87263d373577e7fe
Fred Hohman <fred.hohman@gmail.com>
2016-11-01 01:12:33 -0400

    season and episode grid architecture

--- js/main.js
+++ js/main.js
@@ -1,2 +1,6 @@
-console.log("Javascript loaded!");
+console.log("Javascript loading!");
 
+d3.select("#s1-c1").style("background-color", "red");
+d3.select("#e3-c9").style("background-color", "red");
+
+console.log("Javascript loaded!");
