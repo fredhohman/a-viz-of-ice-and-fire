e7e940fe6502a9ee2b041ca690a933d53049e47d
Fred Hohman <fred.hohman@gmail.com>
2016-11-07 00:12:50 -0500

    second block works, shows colors for each season

--- js/main.js
+++ js/main.js
@@ -13,30 +13,71 @@ function make_episode_color_list() {
     return episode_list;
 }
 
-// testing
-// d3.select("#s1-c1").style("background-color", "red");
-// d3.select("#e3-c9").style("background-color", "rgb(0,0,0)");
+function color_blocks(episode_start, episode_end) {
+    d3.json("../data/color/series.json", function(error, data){
+        console.log("Loaded series.json.");
+        palettes = data['palettes'];
+        // console.log(palettes);
+        var episode_list = make_episode_color_list();
+        // console.log(episode_list);
 
+        for (var episode = episode_start; episode < episode_end; episode++) {
+            for (var color = 0; color < 10; color++) {
 
+                // console.log(episode); 
+                // console.log(color);
+                // console.log(palettes[episode]);
+                // console.log(palettes[episode][color]);
 
-d3.json("../data/color/series.json", function(error, data){
-    console.log("Loaded series.json.");
-    palettes = data['palettes'];
-    console.log(palettes);
-    var episode_list = make_episode_color_list();
-    console.log(episode_list);
-
-    for (var episode = 10; episode < 20; episode++) {
-        for (var color = 0; color < 10; color++) {
-
-            // console.log(episode); 
-            // console.log(color);
-            // console.log(palettes[episode]);
-            // console.log(palettes[episode][color]);
-            console.log(episode_list[color]);
-            d3.select("#" + episode_list[episode%10*10+color]).style("background-color", "rgb(" + String(palettes[episode][color][0]) + "," + String(palettes[episode][color][1]) + "," + String(palettes[episode][color][2]) + ")");
+                // console.log(episode_list[color]);
+                d3.select("#" + episode_list[episode%10*10+color])
+                  .style("background-color", "rgb(" + String(palettes[episode][color][0]) + "," 
+                                                    + String(palettes[episode][color][1]) + "," 
+                                                    + String(palettes[episode][color][2]) + ")");
+            }
         }
-    }
-})
+    })
+}
+
+// Initialize blocks with season 1
+// $(document).ready(function() {
+//     color_blocks(0,10)
+// });
+
+$(document).ready(function() {
+    $("#series").click(function(event) {
+        var series_clicked = event.target.id[1];
+        var episode_start = 0;
+        var episode_end = 0;
+
+        if (series_clicked == "1") {
+            episode_start = 0;
+            episode_end = episode_start + 10;
+        };
+        if (series_clicked == "2") {
+            episode_start = 10;
+            episode_end = episode_start + 10;
+        }
+        if (series_clicked == "3") {
+            episode_start = 20;
+            episode_end = episode_start + 10;
+        };
+        if (series_clicked == "4") {
+            episode_start = 30;
+            episode_end = episode_start + 10;
+        };
+        if (series_clicked == "5") {
+            episode_start = 40;
+            episode_end = episode_start + 10;
+        };
+        if (series_clicked == "6") {
+            episode_start = 50;
+            episode_end = episode_start + 10;
+        };
+        
+        color_blocks(episode_start, episode_end);
+
+    });
+});
 
 console.log("Javascript loaded!");
