b1dd93f818d520bcd6afd65919660724d61295ee
istewart6 <istewart6@gatech.edu>
2016-11-22 18:52:15 -0500

    updated chunk dialogue

--- scripts/chunk_dialogue.py
+++ scripts/chunk_dialogue.py
@@ -1,23 +1,37 @@
 # author: istewart6 (some help from ssoni)
 """
 Code to break subtitle dialogue
-into brief chunks.
+into 60 equal-length chunks per episode.
 """
+from __future__ import division
 import pandas as pd
-import datetime as datetime
+from datetime import datetime
 import re, os
+
+def convert_time(time_):
+    new_time = datetime.strptime(time_, '%H:%M:%S,%f')
+    new_time = new_time.hour * 60 + new_time.minute + new_time.second / 60
+    return new_time
 
 if __name__ == '__main__':
     subtitle_dir = '../data/subtitles/subtitlesInTSV/'
     all_episodes = [f for f in os.listdir(subtitle_dir) 
-                    if re.findall('S[0-9]E[0-9]+', f)]
+                    if re.findall('S[0-9]E[0-9]+.tsv', f)]
     sorted_episodes = sorted(all_episodes)
     episode_data = {e : pd.read_csv(os.path.join(subtitle_dir, e), sep='\t') 
                     for e in sorted_episodes}
-    chunk_length = 2
+    # chunk_length = 2
+    n_chunks = 60
     for e in sorted_episodes:
         e_data = episode_data[e]
-        chunks = e_data.apply(lambda r : int(r['endTime'].split(':')[1]) / chunk_length, axis=1)
+        # print('final datum %s'%(e_data.loc[e_data.index[-1]]['endTime']))
+        end = e_data['endTime'].max()
+        # end = e_data.loc[e_data.index[-1]]['endTime']#.split(',')[0]
+        total = convert_time(end)
+        chunk_length = total / n_chunks
+        # print('about to convert end time data %s'%(e_data['endTime']))
+        chunks = e_data.apply(lambda r : int(convert_time(r['endTime']) / chunk_length), 
+                                axis=1)
         e_data['chunk'] = chunks
         fname = os.path.join(subtitle_dir, '%s.tsv'%(e.split('.tsv')[0]))
         e_data.to_csv(fname, sep='\t', index=False)
