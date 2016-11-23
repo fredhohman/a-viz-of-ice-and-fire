b816043294357592e709d0c92beb823705db09e1
Sandy <sandeepsoni@gatech.edu>
2016-11-22 20:15:10 -0500

    update the words dynamically

--- vis/js/main.js
+++ vis/js/main.js
@@ -160,26 +160,6 @@ $(document).ready(function() {
 
 });
 
-// initializlie text
-/*
-$(document).ready(function() {
-    var color = d3.scaleOrdinal().range(["#020202", "#3c3c3c", "#4b4a4a", "#5e5d5d", "#727171", "#7e7e7e", "#8d8d8d", "#a19f9f", "#b6b5b5", "#C7C6C6"]);//["#48A36D",  "#56AE7C",  "#64B98C", "#72C39B", "#80CEAA", "#80CCB3", "#7FC9BD", "#7FC7C6", "#7EC4CF", "#7FBBCF", "#7FB1CF", "#80A8CE", "#809ECE", "#8897CE", "#8F90CD", "#9788CD", "#9E81CC", "#AA81C5", "#B681BE", "#C280B7", "#CE80B0", "#D3779F", "#D76D8F", "#DC647E", "#E05A6D", "#E16167", "#E26962", "#E2705C", "#E37756", "#E38457", "#E39158", "#E29D58", "#E2AA59", "#E0B15B", "#DFB95C", "#DDC05E", "#DBC75F", "#E3CF6D", "#EAD67C", "#F2DE8A"]);  
-    d3.tsv("data/LIWC_chunk_counts_all_seasons.tsv", function(error, data) { 
-    	textData = data;
-    	var slicedData = sliceData (textData, parseInt(seasonNumber), parseInt(episodeNumber));
-    	var columnNames = d3.keys(slicedData[0]).filter(function(key) {  
-            return key !== "time" && key !== "season" && key !== "episode";
-        }); 
-    	visibilities = columnNames.reduce (function (obj, d){
-    	    obj[d] = d=="positive_affect";
-    	    return obj;
-    	    }, {});
-    	//updateTimePlot (slicedData);
-	initTimePlot (slicedData);
-    });
-});
-*/
-
 ///////////////////////////////////////////////////////////////////////////
 // Main color updating click function
 ///////////////////////////////////////////////////////////////////////////
@@ -253,12 +233,6 @@ $(".episode-block").click(function (event) {
    // console.log (episodeNumber);
 
    // slice the data and call the update method from here
-   //var slicedData = sliceData (textData, parseInt(seasonNumber), parseInt(episodeNumber));
-    //console.log (slicedData);
-   //updateTimePlot (slicedData);
-   console.log (seasonNumber);
-   console.log (episodeNumber);
-   updateLayout (sliceData (textData, seasonNumber, episodeNumber));
-   console.log (episodeNumber);
+   updateScatterPlot (sliceData (textData, seasonNumber, episodeNumber));
 });
 
