5184aa00b641f72be9b50796bfaf5a9a57254e2d
istewart6 <istewart6@gatech.edu>
2016-11-22 19:18:26 -0500

    updating chunk code

--- scripts/chunk_dialogue.py
+++ scripts/chunk_dialogue.py
@@ -13,17 +13,25 @@ def convert_time(time_):
     new_time = new_time.hour * 60 + new_time.minute + new_time.second / 60
     return new_time
 
-if __name__ == '__main__':
+def main():
     subtitle_dir = '../data/subtitles/subtitlesInTSV/'
+    metadata_file = '../vis/data/episode_metadata.csv'
     all_episodes = [f for f in os.listdir(subtitle_dir) 
                     if re.findall('S[0-9]E[0-9]+.tsv', f)]
     sorted_episodes = sorted(all_episodes)
     episode_data = {e : pd.read_csv(os.path.join(subtitle_dir, e), sep='\t') 
                     for e in sorted_episodes}
+    episode_metadata = pd.read_csv(metadata_file)
+    # get lookup 
+    combined_season_episode = ('S' + (episode_metadata['season']).astype(str) + 
+                               'E' + (episode_metadata['episode']).astype(str))
+    runtime_lookup = dict(zip(combined_season_episode, episode_metadata['runtime']))
     # chunk_length = 2
     n_chunks = 60
     for e in sorted_episodes:
         e_data = episode_data[e]
+        season_episode = re.findall('S[0-9]E[0-9]+')
+        runtime = runtime_lookup[e]
         # print('final datum %s'%(e_data.loc[e_data.index[-1]]['endTime']))
         end = e_data['endTime'].max()
         # end = e_data.loc[e_data.index[-1]]['endTime']#.split(',')[0]
@@ -35,3 +43,6 @@ if __name__ == '__main__':
         e_data['chunk'] = chunks
         fname = os.path.join(subtitle_dir, '%s.tsv'%(e.split('.tsv')[0]))
         e_data.to_csv(fname, sep='\t', index=False)
+
+if __name__ == '__main__':
+    main()
