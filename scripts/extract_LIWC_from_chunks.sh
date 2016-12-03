fa4fff290af230074588efda5913068162c15167
istewart6 <istewart6@gatech.edu>
2016-12-03 11:05:20 -0500

    updated LIWC extraction

--- scripts/extract_LIWC_from_chunks.sh
+++ scripts/extract_LIWC_from_chunks.sh
@@ -4,5 +4,5 @@ SUB_FILES=$SUB_DIR/S*E*[0-9].tsv
 for SUB_FILE in $SUB_FILES;
 do
     echo $SUB_FILE
-    python extract_LIWC_from_chunks.py --all_episodes $SUB_FILE &
+    (python extract_LIWC_from_chunks.py --all_episodes $SUB_FILE)&
 done
