8f03a12d5264587ed59da59d7670b920542f980b
istewart6 <istewart6@gatech.edu>
2016-12-06 21:03:45 -0500

    updated house counts

---
+++ scripts/extract_house_counts_from_chunks.py
@@ -0,0 +1,82 @@
+"""
+Extract counts of House member mentions
+from raw text.
+"""
+from clean_extracted_text import clean_text
+from get_LIWC_counts import get_LIWC_counts
+from nltk.tokenize import WordPunctTokenizer
+import os, re
+import pandas as pd
+import argparse
+
+N_CHUNKS=60
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--all_episodes', default=None)
+    parser.add_argument('--house_dir', default='../data/extra_lexicons')
+    args = parser.parse_args()
+    all_episodes = args.all_episodes
+    if(all_episodes is None):
+        subtitle_dir = '../data/subtitles/subtitlesInTSV/'
+        all_episodes = [f for f in os.listdir(subtitle_dir) 
+                        if re.findall('S[0-9]E[0-9]+.tsv', f)]
+    else:
+        all_episodes = all_episodes.split(',')
+        subtitle_dir = os.path.dirname(all_episodes[0])
+        all_episodes = list(map(os.path.basename, all_episodes))
+    sorted_episodes = sorted(all_episodes)
+    episode_data = {e : pd.read_csv(os.path.join(subtitle_dir, e), sep='\t') 
+                    for e in sorted_episodes}
+    house_dir = args.house_dir
+    houses = sorted(os.listdir(house_dir))
+    house_wordlists = {h : [re.compile('^' + l.strip()  + '$')
+                            for l in open(os.path.join(house_dir, h), 'r')] 
+                       for h in houses}
+    TKNZR = WordPunctTokenizer()
+    full_chunk_list = set(range(N_CHUNKS))
+    # we count either the total number of tokens
+    # or the number of unique tokens
+    count_option = 'total'
+    # count_option = 'unique'
+    all_counts = []
+    for e in sorted_episodes:
+        print('processing episode %s'%(e))
+        e_data = episode_data[e]
+        e_name = e.split('.tsv')[0]
+        season = int(re.findall('(?<=S)[0-9]', e_name)[0])
+        episode = int(re.findall('(?<=E)[0-9]+', e_name)[0])
+        e_data.sort_values('chunk', ascending=True)
+        # TODO: insert dummy values for empty chunks
+        empty_chunks = full_chunk_list - set(e_data['chunk'].unique())
+        if(len(empty_chunks) > 0):
+            print('filling %s with empty chunks %s'%
+                  (e_name, empty_chunks))
+            empty_chunk_rows = pd.DataFrame(
+                [{'chunk' : c, 'dialogue' : ''} 
+                 for c in empty_chunks]
+                )
+            e_data = pd.concat([e_data, empty_chunk_rows], 
+                               axis=0)
+        chunk_iter = e_data.groupby('chunk')
+        chunk_text = [clean_text(' '.join(map(str, c[1]['dialogue'].tolist()))) 
+                      for c in chunk_iter]
+        house_counts = {h : [] for h in houses}
+        for t in chunk_text:
+            tokens = TKNZR.tokenize(t)
+            for h in houses:
+                counts = get_LIWC_counts(tokens, LIWC_words=house_wordlists[h])
+                if(count_option == 'total'):
+                    total_counts = sum(counts.values())
+                elif(count_option == 'unique'):
+                    total_counts = len(counts)
+                # TODO: store individual words as well as aggregate counts
+                house_counts[h].append(total_counts)
+        house_counts = pd.DataFrame(house_counts)
+        house_counts['time'] = house_counts.index
+        house_counts['episode'] = episode
+        house_counts['season'] = season
+        all_counts.append(house_counts)
+    all_counts = pd.concat(all_counts, axis=0)
+    chunk_fname = os.path.join(subtitle_dir, 'house_chunk_counts_all_seasons.tsv')
+    all_counts.to_csv(chunk_fname, sep='\t', index=None)
+        
