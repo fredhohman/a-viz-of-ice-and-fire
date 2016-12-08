7ff8184ad82aa1f03f7f57cab5784fe53bf272a4
Crass Talker <Crass Talker>
2016-12-07 20:04:09 -0500

    add title to axes and center them

--- vis/js/textPlot.js
+++ vis/js/textPlot.js
@@ -37,19 +37,6 @@ var pg, catG;
 var catInFocus;
 var circles = {};
 
-/*
-function sliceData (data, season, episode){
-  var slicedData = Array();
-  for(var index = 0; index < data.length; index++) {
-    if(data[index]['season'] == season &&
-       data[index]['episode'] == episode){
-      slicedData.push(data[index]);
-    }
-  }
-  return slicedData;
-}
-*/
-
 function formatData (data, catName){
 	return data.map(function (d){
 		return {"x":parseInt(d.time),
@@ -72,147 +59,9 @@ function findMaxY (data){
 
 	return d3.max (maxYVals);
 }
-/*
-function updateScatterPlot (data){
-    var catName, grp;
-    console.log (data.map (function (d){return parseInt(d.time);}));
-    console.log (d3.extent(data, function(d) { return parseInt(d.time); }));
-    xScale.domain(d3.extent(data, function(d) { return parseInt(d.time); }));
 
-    maxY = findMaxY(data);
-    yScale.domain([0,maxY]);
-
-    // Step 1: Redraw both the axes.
-    svg.select(".y.axis")
-        .transition()
-        .call(yAxis);
-
-    svg.select(".x.axis")
-        .transition()
-        .call(xAxis);
-
-    // Step 2: Enter or update the circles based on new data.
-    for (var i=0; i < categoryNames.length; i++){
-    	catName = categoryNames[i];
-    	if (categoryVisibilities[catName]){
-    		// Select the group corresponding to this category.
-    		grp = svg.selectAll("#spg-"+ catName);
-    		circles[catName] = grp.selectAll("circle")
-    		.data (formatData(data, catName));
-
-    		circles[catName]
-    		.enter()
-    		.append ("circle")
-    		.attr ("r", pointRadius)
-    		.attr ("cx", function (d){
-    			return xScale (d.x);
-    		})
-    		.attr ("cy", function (d){
-    			return yScale(d.y);
-    		})
-            // show relevant words at given timestep
-            .on("mouseover", function(d) {
-                if(d.y > 0) {
-                    if(d.name == catInFocus) {
-                        time = d.time;
-                        // filter data
-                        var relevantData = textTokenData.filter(function(d) {
-                            return d.season == seasonNumber && d.episode == episodeNumber && d.time == time;
-                        })[0];
-
-                        // process data
-                        var parts = relevantData[catInFocus].split(",");
-                        var wordCounter = parts.map (function (entry){
-                            return {
-                                word: entry.split(":")[0],
-                                freq: parseInt(entry.split(":")[1])                 
-                            }
-                        });
-
-                        // TODO: make text actually appear
-                        var xPos = xScale(d.x);
-                        var yPos = yScale(d.y);
-                        tip.transition()
-                            .duration(tooltipTransition)
-                            .style("opacity", 0.9);
-                        tip.html("test </br>")
-                            // .style("left", d3.event.pageX + "px")
-                            // .style("top", (d3.event.pageY - 20) + "px")
-                            .style("left", parseInt(xPos) + "px")
-                            .style("top", parseInt(yPos) + "px");
-
-                        // var tooltip = svg.select("div.tooltip");
-                        // var tooltipWordHeight = 10;
-                        // var tooltipWordBuffer = 5;
-                        
-                        // tooltip.transition()
-                        //         .duration(tooltipTransition)
-                        //         .style("opacity", .9)
-                        //         .attr("height", (tooltipWordHeight + tooltipWordBuffer) * wordCounter.length);
-                        // tooltip
-                        // .html(wordCounter.forEach(function(d) { return d.word + "<br/>"; }))
-                        //     .style("left", xPos + "px")
-                        //     .style("top", yPos + "px");
-                            // .attr("z-index", 1000);
-                        // add text
-                        // var tooltipText = tooltip.selectAll("text.tooltip")
-                        //                     .data(wordCounter);
-                        // tooltipText.enter()
-                        //             .append("text")
-                        //             .attr("class", "tooltip")
-                        //             .attr("x", xPos)
-                        //             .attr("y", function(d, i) {
-                        //                 return yPos + tooltipWordBuffer * 2 + (tooltipWordHeight + tooltipWordBuffer) * i;
-                        //             })
-                        //             .text(function(d) { return d.word; })
-                        //             .style("fill", unfocusColor);
-
-                        // tooltipText.attr("x", xScale(d.x))
-                        //             .attr("y", function(d, i) {
-                        //                 return yPos + tooltipWordBuffer * 2 + (tooltipWordHeight + tooltipWordBuffer) * i;
-                        //             })
-                        //             .text(function(d) { return d.word; })
-                        //             .style("fill", unfocusColor);
-                    }
-
-                }
-            })
-            .on("mouseout", function(d) {
-                tip.transition()
-                    .duration(tooltipTransition)
-                    .style("opacity", 0);
-                // var tooltip = svg.select("div.tooltip");
-                // tooltip.transition()
-                //         .duration(tooltipTransition)
-                //         .style("opacity", 0);
-                // var tooltipText = tooltip.selectAll("text.tooltip");
-                // // console.log(tooltipText);
-                // tooltipText.exit().remove();
-                // tooltip.exit.remove();
-            });
-
-    		circles[catName]
-    		.attr ("cx", function (d){
-    			return xScale (d.x);
-    		})
-    		.attr ("cy", function (d){
-    			return yScale(d.y);
-    		});
-
-			circles[catName].exit().remove();    		
-    	}
-    }
-
-    // Step 2: Flip the visibility of the groups.
-	catG.style ("visibility", function (d, i) {
-	      return categoryVisibilities[d] ? "visible" : "hidden";
-	});
-}
-
-var tip;
-*/
 function initBubblePlot (data){
-    margin = {top: 20, right: 160, bottom: 100, left: 90};
+    margin = {top: 30, right: 160, bottom: 100, left: 90};
     width = 900 - margin.left - margin.right;
     height = 500 - margin.top - margin.bottom;
 
@@ -285,6 +134,11 @@ function initBubblePlot (data){
 
     pg = svg.append ("g")
          .attr ("id", "spg");
+
+    svg.append ("text")
+    .attr ("transform", "translate (" + (width/2) + ", -15)")
+    .style("text-anchor", "middle")
+    .text("Slices (typically one minute each)");
 }
 
 function updateBubblePlot (data){
@@ -383,184 +237,6 @@ function updateBubblePlot (data){
     circles.exit().remove();
 }
 
-/*
-function initScatterPlot (data){
-	// Initialize margins
-	margin = {top: 20, right: 200, bottom: 100, left: 50};
-	width = 900 - margin.left - margin.right;
-    height = 500 - margin.top - margin.bottom;
-    pointRadius = 2;
-    // Define the initial scales and axes
-    xScale = d3.scaleLinear()
-    .range([0, width]);
-
-    yScale = d3.scaleLinear()
-    .range([height, 0]);
-
-    xAxis = d3.axisBottom()
-    .scale(xScale);
-
-	yAxis = d3.axisLeft()
-    .scale(yScale);
-
-    // Step 1: create an svg element with required height and width
-    // The svg is the container element for the plot. All the 
-    // plot elements are grouped together in tag identified by id=textPlot.
-	svg = d3.select("#text")
-	.append("svg")
-	.attr ("width", width + margin.left + margin.right)
-	.attr ("height", height + margin.top + margin.bottom)
-	.append ("g")
-	.attr ("id", "textPlot")
-	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-	// Step 2: add an invisible rectange. This is not really required, 
-	// but is useful for mouse-tracking.
-	svg.append("rect")
-	.attr("width", width)
-	.attr("height", height)                                    
-	.attr("x", 0) 
-	.attr("y", 0)
-	.attr("id", "mouse-tracker")
-	.style("fill", "white");
-
-	// Step 3: Draw the x and the y axis
-	    // draw line graph
-    svg.append("g")
-	  .attr("class", "x axis")
-	  .attr("transform", "translate(0," + height + ")")
-	  .call(xAxis);
-
-	
-    svg.append("g")
-	  .attr("class", "y axis")
-	  .call(yAxis)
-	  .append("text")
-	  .attr("transform", "rotate(-90)")
-	  .attr("y", 6)
-	  .attr("x", -10)
-	  .attr("dy", ".71em")
-	  .style("text-anchor", "end")
-	  .text("Text counts");
-
-	// Read the data and extract the column names
-	categoryNames = d3.keys(data[0]).filter(function(key) {  
-            return key !== "time" && key !== "season" && key !== "episode";
-        });
-
-    categoryVisibilities = categoryNames.reduce (function (obj, d){
-    	obj[d] = d==defaultCategory;
-    	    return obj;
-    	}, {});
-
-    color.domain (categoryNames);
-
-    catInFocus = defaultCategory;
-
-	// Step 4: draw the legend
-	var legendSpace = 450 / categoryNames.length;
-	var legendGroup = svg.append("g")
-	   .attr("class", "legend");
-    var tooltipTransition = 500;
-    var tooltipOffset = 30;
-
-	legendRects = legendGroup.selectAll ("rect")
-	.data (categoryNames)
-	.enter()
-	.append("rect")
-	.attr("width", 10)
-    .attr("height", 10)
-    .attr("x", width + (margin.right/3) - 15)
-    .attr("y", function (d, i) { return (legendSpace)+i*(legendSpace) - 8; })
-    .attr("fill", function (d, i){
-        if(categoryVisibilities[d]) {
-            if(d == catInFocus) {
-                return focusColor;
-            }
-            else { 
-                return unfocusColor;
-            }
-        }
-        else {
-            return invisibleColor;
-        }
-    	//return categoryVisibilities[d] ? color(d) : invisibleColor;
-    })
-    .attr("id", function (d){
-    	return "rect-" + d;
-    })
-    .attr("class", "legend-box")
-    .on("click", function (d){
-    	categoryVisibilities[d] = !categoryVisibilities[d];
-    	d3.selectAll("#rect-" + d).attr("fill", categoryVisibilities[d] ? color(d) : invisibleColor);
-    	updateScatterPlot(sliceData (textData, seasonNumber, episodeNumber));
-        var newCategory = d;
-        // if new category is no longer visible,
-        // assign focus to nearest category
-        if(!categoryVisibilities[d])
-        {
-            var visibleCategories = categoryNames.filter(function(d) {return categoryVisibilities[d];});
-            if(visibleCategories.length > 0){
-                
-                var idx = categoryNames.indexOf(d);
-                var nearestCategory = visibleCategories[0];
-                var nearestDist = categoryNames.length;
-                for(var i = 0; i < visibleCategories.length; i++){
-                    var category = visibleCategories[i];
-                    var dist = Math.abs(idx - categoryNames.indexOf(category));
-                    console.log(category);
-                    console.log(dist);
-                    if(dist < nearestDist){
-                        nearestCategory = category;
-                        nearestDist = dist;
-                        console.log(nearestCategory);
-                    }
-                }
-                console.log(nearestCategory);
-                newCategory = nearestCategory;
-            }
-            
-        }
-        updateFocus(newCategory);
-    })
-    .on("mouseover", function (d){
-        // console.log(d);
-        updateFocus(d);
-    });
-
-    legendLabels = legendGroup.selectAll ("text")
-    .data (categoryNames)
-    .enter()
-    .append("text")
-    .attr("x", width + (margin.right/3)) 
-    .attr("y", function (d, i) { return (legendSpace)+i*(legendSpace);})
-    .text(function(d) { return d; });
-
-    pg = svg.append ("g")
-         .attr ("id", "spg");
-
-    catG = pg.selectAll ("g")
-    .data (categoryNames)
-    .enter()
-    .append("g")
-    .attr("id", function (d){ 
-    	return "spg-" + d;
-    })
-    .attr("class", "spg-cats");
-
-    // make tooltip?
-    //tip = d3.select("g#spg")
-    tip = svg.append("div")
-            .attr("class", "tooltip")
-            .style("opacity", 0);
-
-    // svg.append("rect")
-    //     .attr("class", "tooltip")
-    //     .style("opacity", 0);
-}
-*/
-
-
 function initBarPlot (data){
     wordlistBarWidth = 200;
     wordlistBarHeight = 15;
@@ -632,7 +308,7 @@ function initBarPlot (data){
 }
 
 function initHouseBubblePlot(houseCountData) {
-    var margin = {top: 20, right: 160, bottom: 100, left: 150};
+    var margin = {top: 30, right: 160, bottom: 100, left: 150};
     var width = 900 - margin.left - margin.right;
     var height = 500 - margin.top - margin.bottom;
 
@@ -711,7 +387,13 @@ function initHouseBubblePlot(houseCountData) {
     svgHouses.append ("g")
             .attr ("id", "spg");
 
-    // console.log(houseCountData);
+    svgHouses.append ("text")
+    .attr ("transform", "translate (" + (width/2) + ", -15)")
+    .style("text-anchor", "middle")
+    .text("Episodes");
+
+    // 
+    console.log(houseCountData);
     var houseDataDots = data2DotPlotRepresentationBasic(houseCountData, houses);
     console.log(houseDataDots.length);
     // houseDataDots = houseDataDots.filter(function(d) { 
@@ -768,6 +450,7 @@ function initHouseBubblePlot(houseCountData) {
             d3.select(this).attr ("fill", unfocusColor).attr("stroke", unfocusBorderColor); 
             d3.select(this).style("z-index", "1");  // Does not work; should take a look.
     });
+
 
     circles
     .attr ("r", function (d){
